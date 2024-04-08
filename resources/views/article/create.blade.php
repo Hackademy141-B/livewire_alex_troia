d80a03c079a36f9523d5c2edb30fc89674ea3f48
Alex Troia <brian.troia@icloud.com>
2024-04-08 19:18:06 +0200

    iniziato progetto ma ancora da finire

---
+++ resources/views/article/create.blade.php
@@ -0,0 +1,12 @@
+<x-layout> 
+    <div class="container bg-success">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6">
+               <h1 class="text-center">
+                Primo progetto Livewire
+               </h1>
+            </div>
+        </div>
+    </div>
+
+</x-layout>
